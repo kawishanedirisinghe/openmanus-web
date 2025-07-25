b3691b348f8423796541c35f21e41dec1225e701
kawishanedirisinghe <kawishanedirisinghe@gmail.com>
2025-07-25 19:07:24 +0530

    Update logger.py

--- app/logger.py
+++ app/logger.py
@@ -1,6 +1,6 @@
 import sys
 from datetime import datetime
-
+import queue
 from loguru import logger as _logger
 
 from app.config import PROJECT_ROOT
@@ -8,6 +8,13 @@ from app.config import PROJECT_ROOT
 
 _print_level = "INFO"
 
+# 创建线程安全的队列用于存储日志
+log_queue = queue.Queue()
+
+# 自定义 Sink 将日志写入队列
+def log_sink(message):
+    """将 loguru 日志消息推送到队列"""
+    log_queue.put(message)
 
 def define_log_level(print_level="INFO", logfile_level="DEBUG", name: str = None):
     """Adjust the log level to above level"""
@@ -15,13 +22,11 @@ def define_log_level(print_level="INFO", logfile_level="DEBUG", name: str = None
     _print_level = print_level
 
     current_date = datetime.now()
-    formatted_date = current_date.strftime("%Y%m%d%H%M%S")
-    log_name = (
-        f"{name}_{formatted_date}" if name else formatted_date
-    )  # name a log with prefix name
-
+    formatted_date = current_date.strftime("%Y%m%d")
+    log_name = "root"
     _logger.remove()
     _logger.add(sys.stderr, level=print_level)
+    _logger.add(log_sink, level=print_level)
     _logger.add(PROJECT_ROOT / f"logs/{log_name}.log", level=logfile_level)
     return _logger
 
